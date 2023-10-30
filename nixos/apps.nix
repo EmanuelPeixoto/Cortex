002e4536890410a516e300a8c375d79c3f1e19ad
EmanuelPeixoto <leunamepeixoto@gmail.com>
2023-10-30 11:24:45 -0300

    added to repo

---
+++ nixos/apps.nix
@@ -0,0 +1,61 @@
+{lib, config, pkgs, ... }:
+{
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+  curl
+  elixir
+  ffmpeg
+  font-manager
+  gcc
+  gdb
+  gh
+  git
+  glibc
+  glxinfo
+  gnat
+  gnumake
+  gnupg
+  gparted
+  home-manager
+  htop
+  iftop
+  insomnia
+  jdk
+  kate
+  killall
+  libpulseaudio
+  libreoffice
+  lm_sensors
+  lshw
+  maim
+  neofetch
+  neovim
+  nload
+  pamixer
+  paprefs
+  pavucontrol
+  pciutils
+  pkg-config
+  playerctl
+  postgresql
+  pulseaudioFull
+  ranger
+  speedtest-cli
+  steam-run
+  steam-tui
+  stress
+  tetex
+  texstudio
+  unzip
+  usbutils
+  vlc
+  vscode
+  wget
+  xclip
+  xfce.thunar
+  yt-dlp
+  zip
+  ];
+
+
+}
