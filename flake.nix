1cb5024c176ce149563bc637755d0cab27a3e7e8
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2023-11-06 16:10:41 -0300

    added home-manager to flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,18 @@
   description = "My NixOS config in flake";
 
 
-  inputs ={
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixunstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixunstable";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-Linux";
 
@@ -25,6 +32,10 @@
           ./nixos/configuration.nix
         ];
       };
+			emanuel = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+				modules = [ ./home-manager/home.nix ];
+			};
     };
   };
 }
