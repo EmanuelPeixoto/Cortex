bec462fa271f93b0f5ee62a725380025eb07e413
EmanuelPeixoto <leunamepeixoto@gmail.com>
2023-11-23 17:53:43 -0300

    converted all tabs to spaces

--- nixos/intel2.nix
+++ nixos/intel2.nix
@@ -1,8 +1,8 @@
 {lib, config, pkgs, ... }:
 {
- 
+
   services.xserver.videoDrivers = [ "modesetting" ];
- 
+
   #services.xserver.xrandrHeads = [ { output = "LVDS1"; primary = true; } "VGA1" ];
 
   /*nixpkgs.config.packageOverrides = pkgs: {
@@ -16,11 +16,11 @@
     driSupport32Bit = true;
 
     package = (pkgs.mesa.override {
-      galliumDrivers = [ "crocus" "swrast" ]; 
+      galliumDrivers = [ "crocus" "swrast" ];
       vulkanDrivers = [ "intel" ];
       enableGalliumNine = false;
     }).drivers;
-    
+
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
