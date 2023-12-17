c4a35650d906451d89f905163a361ee1ce7c0b63
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2023-12-17 20:35:24 -0300

    moved all user apps to home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,101 +1,33 @@
-{ config, pkgs, ... }:
+{lib, config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "emanuel";
   home.homeDirectory = "/home/emanuel/";
 
   imports =
     [
-      #./nextcloud.nix
-      ./terminal.nix
+      ./apps.nix
+      ./i3.nix
+      ./nextcloud.nix
       ./nvim.nix
       ./polybar/polybar.nix
-      ./i3.nix
+      ./terminal.nix
     ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
+  home.stateVersion = "22.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.sessionVariables = {
   EDITOR = "nvim";
   BROWSER = "firefox";
   TERMINAL = "alacritty";
   };
 
-  # Allow unfree packages
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
-    };
-  };
-  home.packages = with pkgs; [
-    aircrack-ng
-    alacritty
-    arduino
-    arduino-cli
-    audacity
-    discord
-    dmenu
-    firefox
-    gimp
-    git
-    go
-    gopls
-    gotty
-    htop
-    jetbrains.goland
-    kate
-    mixxx
-    mpd-mpris
-    ncmpcpp
-    nodejs_20
-    obs-studio
-    openshot-qt
-    pciutils
-    qbittorrent
-    telegram-desktop
-    thunderbird
-    tor
-    tor-browser-bundle-bin
-    ventoy-full
-    vlc
-    warsow
-    wine
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
 
   # Config MPD
   services.mpd = {
     enable = true;
     musicDirectory = "/home/emanuel/Musicas";
+    network.listenAddress = "any";
     extraConfig = ''
       audio_output {
         type "pulse"
@@ -103,9 +35,6 @@
         server "127.0.0.1" # add this line - MPD must connect to the local sound server
       }
     '';
-
-    # Optional:
-    network.listenAddress = "any"; # if you want to allow non-localhost connections
   };
 
   services.mpd-mpris.enable = true;
@@ -135,9 +64,6 @@
   #  /etc/profiles/per-user/emanuel/etc/profile.d/hm-session-vars.sh
   #
   # if you don't want to manage your shell through Home Manager.
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
