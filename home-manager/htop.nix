4122194e0b4bde42c8b811306db196f424713ae0
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2023-12-19 23:57:04 -0300

    added htop config

---
+++ home-manager/htop.nix
@@ -0,0 +1,36 @@
+{lib, config, pkgs, ... }:
+{
+
+  programs.htop.enable = true;
+  programs.htop.settings = {
+    show_cpu_frequency = true;
+    show_cpu_temperature = true;
+    show_program_path = false;
+    screen_tabs = true;
+    delay = 5;
+    fields = with config.lib.htop.fields; [
+      PID
+      USER
+      #PRIORITY
+      #NICE
+      M_SIZE
+      #M_RESIDENT
+      #M_SHARE
+      #STATE
+      PERCENT_CPU
+      PERCENT_MEM
+      TIME
+      COMM
+          ];
+  } // (with config.lib.htop; leftMeters [
+    (bar "AllCPUs")
+    (bar "Memory")
+  ]) // (with config.lib.htop; rightMeters [
+    (text "Uptime")
+    (text "Battery")
+    (text "Systemd")
+    (bar "Swap")
+  ]);
+
+
+}
