73e8ddd4119e700ce50e71bf1860fd84898e54fa
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2023-12-21 21:03:14 -0300

    moved picom to user config

--- nixos/intel.nix
+++ nixos/intel.nix
@@ -8,22 +8,23 @@
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
 
-  services.picom = {
-    enable = true;
-    vSync = true;
-  };
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
 
   hardware.opengl = {
     enable = true;
 
     driSupport = true;
     driSupport32Bit = true;
+    setLdLibraryPath = true;
+    #s3tcSupport = true;
+
 
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
+      libGL
     ];
   };
 
