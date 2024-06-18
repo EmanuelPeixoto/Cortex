a12473445e9ea0dd65e77cc0782bdaa14ef4a54d
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-06-18 15:32:05 -0300

    removed old alias

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -10,7 +10,6 @@
 
     shellAliases = {
       motd = "bash /home/emanuel/NixOS/home-manager/motd.sh";
-      bar = "eww close-all && home-manager switch --flake ~/NixOS && eww open bar";
       ssh = "kitten ssh";
     };
     oh-my-zsh = {
