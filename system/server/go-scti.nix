9849269e0ece944c57ca778181465bb910d11a4e
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-07-13 17:26:19 -0300

    go scti working

--- system/server/go-scti.nix
+++ system/server/go-scti.nix
@@ -1,12 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  Path = "/SCTI/src";
+in
 {
   systemd.services.goscti = {
     enable = true;
     description = "Go SCTI service";
     serviceConfig = {
-      ExecStart = "/home/emanuel/SCTI/src/main";
-      # StateDirectory = "GoSCTI";
-      Type = "forking";
+      ExecStart = ''${pkgs.zsh}/bin/zsh -c "cd ${Path} && ${pkgs.go}/bin/go run ." '';
+      User = "emanuel";
+      Group = "users";
+      Restart = "on-failure";
+      RestartSec = "5s";
     };
     wantedBy = [ "multi-user.target" ];
   };
