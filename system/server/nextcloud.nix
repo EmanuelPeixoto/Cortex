728059f2f481d339515d523845cba0c899ecc5d8
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-07-18 22:36:19 -0300

    nextcloud PHP max childs

--- system/server/nextcloud.nix
+++ system/server/nextcloud.nix
@@ -7,6 +7,14 @@
     home = "/var/lib/nextcloud";
     package = pkgs.nextcloud29;
     maxUploadSize = "10240M";
+    poolSettings = {
+      pm = "dynamic";
+      "pm.max_children" = "64";
+      "pm.max_requests" = "500";
+      "pm.max_spare_servers" = "4";
+      "pm.min_spare_servers" = "2";
+      "pm.start_servers" = "2";
+    };
     settings = {
       log_type = "file";
       logLevel = 1;
