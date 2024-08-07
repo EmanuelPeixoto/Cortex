2d9f6ef2c50667ef6bbfbcc85e7a08306308b231
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-08-07 15:37:10 -0300

    eww bar date and battery updates

--- hm/note/eww/bar/scripts/volume.sh
+++ hm/note/eww/bar/scripts/volume.sh
@@ -7,8 +7,7 @@ esac
 
 if [[ "$command" == "-i" || "$command" == "-d" ]]; then
   pamixer $command 1 --allow-boost --set-limit 125
-  exit
 else
-    pamixer --get-volume-human
-    exit
+  pamixer --get-volume-human
 fi
+exit
