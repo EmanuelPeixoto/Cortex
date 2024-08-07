902d94a5140f03969d23988d95312eefaabe9823
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-08-07 15:35:51 -0300

    changed pulseaudio to pipewire

--- system/note/sound.nix
+++ system/note/sound.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
+  # hardware.pulseaudio.extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
+  environment.systemPackages = with pkgs; [
+    pamixer
+    pavucontrol
+  ];
   security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 }
