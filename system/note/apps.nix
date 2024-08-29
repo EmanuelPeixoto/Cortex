b948e7df2f02db8d05969f8383be77a5262ecb7c
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-08-28 22:26:59 -0300

    Added app description (system)

--- system/note/apps.nix
+++ system/note/apps.nix
@@ -2,12 +2,12 @@
 {
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-    git
-    home-manager
-    nh
-    nix-output-monitor
-    nvd
-    powertop
+    git                       # Distributed version control system
+    home-manager              # A Nix-based user environment configurator
+    nh                        # Yet another nix cli helper
+    nix-output-monitor        # Processes output of Nix commands to show helpful and pretty information
+    nvd                       # Nix/NixOS package version diff tool
+    powertop                  # Analyze power consumption on Intel-based laptops
   ];
 
 
