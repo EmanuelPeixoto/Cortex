e96fc58e1c30eac97f0b60d23832492af19e626c
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-09-08 15:32:15 -0300

    personalized ly

--- system/note/ly.nix
+++ system/note/ly.nix
@@ -1,9 +1,20 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+  ];
+
   services.displayManager.ly = {
     enable = true;
     settings = {
       waylandsessions = "${config.users.users.emanuel.home}/.nix-profile/share/wayland-sessions";
+      animation = "matrix";
+      clock = "%d/%m/%y - %R";
+      xinitrc = "null";
+      brightness_down_cmd = "${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+      brightness_down_key = "F5";
+      brightness_up_cmd = "${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
+      brightness_up_key = "F6";
     };
   };
 }
