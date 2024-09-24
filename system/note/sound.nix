be9eedebd2953c7dc9f9cbb26e5d27eea33e734b
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-09-24 11:29:20 -0300

    moved all sound system to pipewire

--- system/note/sound.nix
+++ system/note/sound.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    pamixer
-    pavucontrol
+    pwvucontrol
   ];
 
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
+  services.pipewire.enable = true;
 }
