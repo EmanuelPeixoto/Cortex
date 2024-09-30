5faac2bb3e039b4b15f7809913df73f79b8bb20e
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-09-30 09:00:21 -0300

    added lo to internet interface in hotspot

--- system/note/hotspot.nix
+++ system/note/hotspot.nix
@@ -2,10 +2,10 @@
   services.create_ap = {
     enable = false;
     settings = {
-      INTERNET_IFACE = "";
+      INTERNET_IFACE = "lo";
       WIFI_IFACE = "wlp1s0";
       SSID = "";
       PASSPHRASE = "";
+      };
     };
-  };
-}
+  }
