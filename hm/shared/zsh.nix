27b5586df1e009f3034ddc1912503ed4a8c1ded8
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-10-02 10:51:55 -0300

    changed zsh.nix file path

--- hm/shared/zsh.nix
+++ hm/shared/zsh.nix
@@ -6,10 +6,10 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    initExtra = "zsh ${config.home.homeDirectory}/Cortex/hm/motd.sh";
+    initExtra = "zsh ${config.home.homeDirectory}/Cortex/hm/shared/motd.sh";
 
     shellAliases = {
-      motd = "bash ${config.home.homeDirectory}/Cortex/hm/motd.sh";
+      motd = "bash ${config.home.homeDirectory}/Cortex/hm/shared/motd.sh";
       ssh = "kitten ssh";
     };
     oh-my-zsh = {
