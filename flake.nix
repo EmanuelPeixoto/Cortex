d06d0a6e454eb680701071c8f9f4a5b093f77613
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-10-02 11:53:55 -0300

    test

--- flake.nix
+++ flake.nix
@@ -8,9 +8,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
-  outputs = { nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, lexis, zen-browser, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -22,17 +21,22 @@
     let
       hm = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./hm/minimal ];
-        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./hm/minimal
+          {
+            # Garantir que inputs esteja disponível em todos os módulos
+            _module.args = {
+              inherit inputs;
+            };
+          }
+        ];
       };
     in
     pkgs.mkShell {
       packages = [ hm.activationPackage ];
-
       shellHook = ''
         # Activate home-manager
         ${hm.activationPackage}/activate
-
         # Use zsh shell
         if [ -x "$(command -v zsh)" ]; then
           exec zsh
@@ -61,13 +65,25 @@
     homeConfigurations = {
       note = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./hm/note ];
-        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./hm/note
+          {
+            _module.args = {
+              inherit inputs;
+            };
+          }
+        ];
       };
       server = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./hm/server ];
-        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./hm/server
+          {
+            _module.args = {
+              inherit inputs;
+            };
+          }
+        ];
       };
     };
   };
