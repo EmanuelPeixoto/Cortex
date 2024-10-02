e39e724db4346c9cd74fd4ec23a1036f07b5e566
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-10-02 11:16:37 -0300

    added minimal config, for shell use

--- flake.nix
+++ flake.nix
@@ -1,23 +1,22 @@
 {
-  description = "NixOS flake configuration";
-
+  description = "My NixOS flake, with home-manager, system (server and notebook) and minimal configuration to be used with shell";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
     lexis.url = "github:EmanuelPeixoto/Lexis";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
   let
     system = "x86_64-linux";
-
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
     };
-
   in {
     nixosConfigurations = {
       NixOS-Note = nixpkgs.lib.nixosSystem {
@@ -28,6 +27,7 @@
         ];
         specialArgs = { inherit inputs; };
       };
+
       NixOS-Server = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
@@ -36,19 +36,27 @@
         ];
         specialArgs = { inherit inputs; };
       };
-
     };
+
     homeConfigurations = {
       note = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./hm/note ];
         extraSpecialArgs = { inherit inputs; };
       };
+
       server = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./hm/server ];
         extraSpecialArgs = { inherit inputs; };
       };
+
+      minimal = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./hm/minimal ];
+      };
     };
+
+    defaultPackage.${system} = self.homeConfigurations.minimal.activationPackage;
   };
 }
---
+++ hm/minimal/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ../shared/btop.nix
+    ../shared/git.nix
+    ../shared/lexis.nix
+    ../shared/yazi.nix
+    ../shared/zsh.nix
+  ];
+
+  home.username = "emanuel";
+  home.homeDirectory = "/home/emanuel";
+  home.stateVersion = "24.05";
+
+  # Programas básicos
+  home.packages = with pkgs; [
+    lazygit                   # Simple terminal UI for git commands
+  ];
+
+  programs.home-manager.enable = true;
+}
