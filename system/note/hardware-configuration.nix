2451d46bb9b8b265eccc858a024152f5acd84617
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-10-11 08:38:13 -0300

    8GB swap

--- system/note/hardware-configuration.nix
+++ system/note/hardware-configuration.nix
@@ -26,7 +26,7 @@
 
   swapDevices = [ {
     device = "/var/lib/swapfile";
-    size = 4*1024;
+    size = 8*1024;
   } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
