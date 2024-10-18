8c223bed028a16a34c04e6c0aae392ca27fbcad8
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-10-18 19:11:40 -0300

    temp

--- system/note/temp.nix
+++ system/note/temp.nix
@@ -1,14 +1,35 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    jetbrains.idea-ultimate
-    docker-compose
+    jetbrains.idea-ultimate # scti poo
+    docker-compose # scti
+    maven # scti poo
+    jdk17 # scti poo
+    dia # ausberto
+    postman # scti poo
   ];
 
+  #scti
   virtualisation.docker.enable = true;
+
+  #joao
   virtualisation.waydroid.enable = true;
 
+
+# virtual box
   boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" ];
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "emanuel" ];
+
+    services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser auth-method
+      local all      all    trust
+      #type database DBuser Address      auth-method
+      host  all      all    127.0.0.1/32 trust
+      host  all      all    ::1/128      trust
+    '';
+  };
 }
