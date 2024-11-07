1558dc9ca3400326416c6ae98240964aa55f50d3
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-11-07 14:29:46 -0300

    Postgres moved from temp to own file

--- system/note/temp.nix
+++ system/note/temp.nix
@@ -1,16 +1,15 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    jetbrains.idea-ultimate # scti poo
-    docker-compose # scti
-    maven # scti poo
-    jdk17 # scti poo
-    dia # ausberto
-    postman # scti poo
+    # jetbrains.idea-ultimate # scti poo
+    # docker-compose # scti
+    # maven # scti poo
+    # jdk17 # scti poo
+    # postman # scti poo
   ];
 
   #scti
-  virtualisation.docker.enable = true;
+  # virtualisation.docker.enable = true;
 
   #joao
   virtualisation.waydroid.enable = true;
@@ -20,16 +19,4 @@
   boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" ];
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "emanuel" ];
-
-    services.postgresql = {
-    enable = true;
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database DBuser auth-method
-      local all      all    trust
-      #type database DBuser Address      auth-method
-      host  all      all    127.0.0.1/32 trust
-      host  all      all    ::1/128      trust
-    '';
-  };
 }
