082913a5424e0279ee13acd0d7de4b3576ed2356
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-11-19 11:05:33 -0300

    removed minimal config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "My NixOS flake";
+
   inputs = {
     ags.url = "github:aylur/ags";
     home-manager.url = "github:nix-community/home-manager";
@@ -10,45 +11,33 @@
     sops-nix.url = "github:Mic92/sops-nix";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
+
   outputs = { nixpkgs, nixpkgs-stable, home-manager, sops-nix, ... }@inputs:
-  let
-    systems = {
-      x86_64-linux = "x86_64-linux";
-      aarch64-linux = "aarch64-linux";
-    };
-    overlay-stable = final: prev: {
-      stable = import nixpkgs-stable {
-        system = prev.system;
-        config.allowUnfree = true;
+    let
+      systems = {
+        x86_64-linux = "x86_64-linux";
+        aarch64-linux = "aarch64-linux";
       };
-    };
-    pkgsForSystem = system: import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-      overlays = [ overlay-stable ];
-    };
-    pkgs = {
-      x86_64-linux = pkgsForSystem systems.x86_64-linux;
-      aarch64-linux = pkgsForSystem systems.aarch64-linux;
-    };
-  in {
-    devShells.${systems.x86_64-linux}.default =
-      let
-        hm = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs.x86_64-linux;
-          modules = [ ./hm/minimal ];
-          extraSpecialArgs = { inherit inputs; };
+
+      overlay-stable = final: prev: {
+        stable = import nixpkgs-stable {
+          system = prev.system;
+          config.allowUnfree = true;
         };
-      in
-      pkgs.x86_64-linux.mkShell {
-        packages = [ hm.activationPackage ];
-        shellHook = ''
-          ${hm.activationPackage}/activate
-          if [ -x "$(command -v zsh)" ]; then
-          exec zsh
-          fi
-        '';
       };
+
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ overlay-stable ];
+      };
+
+      pkgs = {
+        x86_64-linux = pkgsForSystem systems.x86_64-linux;
+        aarch64-linux = pkgsForSystem systems.aarch64-linux;
+      };
+
+    in {
       nixosConfigurations = {
         NixOS-Note = nixpkgs.lib.nixosSystem {
           system = systems.x86_64-linux;
@@ -59,6 +48,7 @@
           ];
           specialArgs = { inherit inputs; };
         };
+
         NixOS-Server = nixpkgs.lib.nixosSystem {
           system = systems.x86_64-linux;
           modules = [
@@ -69,17 +59,20 @@
           specialArgs = { inherit inputs; };
         };
       };
+
       homeConfigurations = {
         note = home-manager.lib.homeManagerConfiguration {
           pkgs = pkgs.x86_64-linux;
           modules = [ ./hm/note ];
           extraSpecialArgs = { inherit inputs; };
         };
+
         server = home-manager.lib.homeManagerConfiguration {
           pkgs = pkgs.x86_64-linux;
           modules = [ ./hm/server ];
           extraSpecialArgs = { inherit inputs; };
         };
+
         rpi3 = home-manager.lib.homeManagerConfiguration {
           pkgs = pkgs.aarch64-linux;
           modules = [ ./hm/rpi3 ];
@@ -87,4 +80,4 @@
         };
       };
     };
-  }
+}
