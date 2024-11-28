f193467b5dff0ad1e9c69cb44fca153763c52295
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-11-27 23:48:00 -0300

    updated flake, modified temp

--- system/note/temp.nix
+++ system/note/temp.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    jetbrains.idea-ultimate # scti poo
     # docker-compose # scti
     # maven # scti poo
-    jdk17 # scti poo
     # postman # scti poo
     chromium
   ];
