66fb0ce2f33b6a2d3cb3da15a994ea9523613399
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-12-12 18:53:42 -0300

    updated flake.nix, added iso build

---
+++ system/note/iso.nix
@@ -0,0 +1,13 @@
+{ lib, modulesPath, ... }:
+{
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    ./default.nix
+  ];
+
+  boot.loader.timeout = lib.mkForce 10;
+  boot.plymouth.enable = lib.mkForce false;
+  networking.networkmanager.enable = lib.mkForce false;
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+}
