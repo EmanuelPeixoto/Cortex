335f38829e2e9c2a5bc47c0f042fc1d64736279c
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-12-18 11:09:56 -0300

    added apps description (server)

--- hm/server/default.nix
+++ hm/server/default.nix
@@ -18,23 +18,23 @@
     };
 
     packages = with pkgs; [
-      curl
-      ffmpeg
-      gcc
-      iftop
-      lazygit
-      lm_sensors
-      neofetch
-      nload
-      pciutils
-      progress
-      speedtest-cli
-      unzip
-      ventoy-full
-      wget
-      wl-clipboard
-      yt-dlp
-      zip
+      curl                      # Command line tool for transferring files with URL syntax
+      ffmpeg                    # Complete, cross-platform solution to record, convert and stream audio and video
+      gcc                       # GNU Compiler Collection
+      iftop                     # Display bandwidth usage on a network interface
+      lazygit                   # Simple terminal UI for git commands
+      lm_sensors                # Tools for reading hardware sensors
+      neofetch                  # Fast, highly customizable system info script
+      nload                     # Monitors network traffic and bandwidth usage with ncurses graphs
+      pciutils                  # Collection of programs for inspecting and manipulating configuration of PCI devices
+      progress                  # Tool that shows the progress of coreutils programs
+      speedtest-cli             # Command line interface for testing internet bandwidth using speedtest.net
+      unzip                     # Extraction utility for archives compressed in .zip format
+      ventoy                    # New Bootable USB Solution
+      wget                      # Tool for retrieving files using HTTP, HTTPS, and FTP
+      wl-clipboard              # Command-line copy/paste utilities for Wayland
+      yt-dlp                    # Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
+      zip                       # Compressor/archiver for creating and modifying zipfiles
     ];
   };
 
