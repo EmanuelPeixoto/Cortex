b5f2908b8947ac77134c999c6ec3224086925c34
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-12-27 20:11:51 -0300

    removed SOPS

--- system/server/ssh.nix
+++ system/server/ssh.nix
@@ -1,20 +1,4 @@
-{ config, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    age
-    sops
-  ];
-
-  sops = {
-    defaultSopsFile = ./secrets/sshkeys.enc.yaml; # sops -e sshkeys.yaml > sshkeys.enc.yaml
-    age.keyFile = "${config.users.users.emanuel.home}/.config/sops/age/keys.txt";
-    defaultSopsFormat = "yaml";
-    secrets."ssh_authorized_keys" = {
-      owner = config.users.users.emanuel.name;
-      mode = "0400";
-    };
-  };
-
   services.openssh = {
     enable = true;
     ports = [ 44 ];
@@ -24,8 +8,4 @@
       PasswordAuthentication = false;
     };
   };
-
-  users.users.emanuel.openssh.authorizedKeys.keyFiles = [
-    config.sops.secrets."ssh_authorized_keys".path
-  ];
 }
