87b17a347ff9f390673acc5a3fe36e1c19054239
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-12-29 15:52:06 -0300

    yazi zsh integrattion being used

--- hm/shared/zsh.nix
+++ hm/shared/zsh.nix
@@ -6,14 +6,13 @@ in
 {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     initExtra = "${pkgs.zsh}/bin/zsh ${motd}/bin/motd";
 
     shellAliases = {
       catfolder = "find . -type f -exec echo \"=== {} ===\" \\; -exec cat {} \\;";
-      cfg = "${pkgs.yazi}/bin/yazi ${config.home.homeDirectory}/.config/Cortex";
+      cfg = "yy ${config.home.homeDirectory}/.config/Cortex";
       motd = "${pkgs.zsh}/bin/zsh ${motd}/bin/motd";
       yt-dlp-menu = "${pkgs.zsh}/bin/zsh ${yt-dlp}/bin/yt-dlp";
     };
