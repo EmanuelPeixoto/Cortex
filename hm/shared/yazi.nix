b0f8feb9d4a0464b36173815c5027a86cf8fc0f4
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-12-29 15:52:06 -0300

    yazi zsh integrattion being used

--- hm/shared/yazi.nix
+++ hm/shared/yazi.nix
@@ -2,14 +2,5 @@
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
-    keymap = {
-      manager.prepend_keymap = [
-        {
-          on = ["<C-s>"];
-          run = ''shell "$SHELL" --block --confirm'';
-          desc = "Open shell here";
-        }
-      ];
-    };
   };
 }
