35aa32c739877362219dd8422e0e79e401381b43
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-12-29 21:17:50 -0300

    wallpaper rotation on hyprland start

--- hm/note/scripts/hyprland_exec.nix
+++ hm/note/scripts/hyprland_exec.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:
-
+{ config, pkgs, ... }:
+let
+  wallpaper = import ./wallpaper.nix { inherit config pkgs; };
+in
 pkgs.writeShellScriptBin "hyprland_exec" ''
   ${pkgs.swww}/bin/swww restore || echo "Failed to set wallpaper"
   ${pkgs.eww}/bin/eww open bar1 || echo "Failed to open bar1"
   ${pkgs.eww}/bin/eww open bar2 || echo "Failed to open bar2"
+  ${wallpaper}/bin/wallpaper
 ''
