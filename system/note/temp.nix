27628fb9b2b64d74d1a15e90c993924117c0ccfc
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-12-30 15:47:33 -0300

    temp

--- system/note/temp.nix
+++ system/note/temp.nix
@@ -1,21 +1,11 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # docker-compose # scti
-    # maven # scti poo
-    # postman # scti poo
     chromium
   ];
 
-  services.xserver.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  # virtualisation.docker.enable = true;
+  # services.xserver.enable = true;
+  # services.xserver.desktopManager.xfce.enable = true;
 
   #joao
   virtualisation.waydroid.enable = true;
@@ -43,5 +33,4 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 80 ];
-
 }
