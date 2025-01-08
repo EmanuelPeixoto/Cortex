a974a1cda7e73c7ee72c66a56126ac39894cacac
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-01-08 13:29:49 -0300

    nix twilight set as default app

--- hm/note/default-apps.nix
+++ hm/note/default-apps.nix
@@ -16,10 +16,9 @@
       "text/plain" = "nvim.desktop";
       "video/mp4" = "vlc.desktop";
       "video/x-matroska" = "vlc.desktop";
-      "x-scheme-handler/about" = "zen.desktop";
-      "x-scheme-handler/chrome" = "zen.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
+      "x-scheme-handler/chrome" = "userapp-Zen Twilight-IXMMZ2.desktop";
+      "x-scheme-handler/http" = "userapp-Zen Twilight-IXMMZ2.desktop";
+      "x-scheme-handler/https" = "userapp-Zen Twilight-IXMMZ2.desktop";
       "x-scheme-handler/magnet" = "qbittorrent.desktop";
       "x-scheme-handler/mailto" = "userapp-Thunderbird-PO8FT2.desktop";
     };
