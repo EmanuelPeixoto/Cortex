221fbee243177ee4424a1de8b5f4679e7724a5dd
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2025-01-16 15:15:15 -0300

    removed qbittorrent and phpsql from nginx

--- system/server/nginx.nix
+++ system/server/nginx.nix
@@ -52,43 +52,6 @@
               }
             '';
           };
-
-          "^~/phpmysql" = {
-            root = "/var/www/";
-            index = "index.html index.php";
-            extraConfig = ''
-              location ~ \.php$ {
-                include ${pkgs.nginx}/conf/fastcgi.conf;
-                fastcgi_pass unix:${config.services.phpfpm.pools.one.socket};
-              }
-            '';
-          };
-
-          "^~/torrent/".extraConfig = ''
-            proxy_pass         http://127.0.0.1:8080/;
-            proxy_http_version 1.1;
-
-            proxy_set_header   Host               127.0.0.1:8080;
-            proxy_set_header   X-Forwarded-Host   $http_host;
-            proxy_set_header   X-Forwarded-For    $remote_addr;
-
-            # not used by qBittorrent
-            #proxy_set_header   X-Forwarded-Proto  $scheme;
-            #proxy_set_header   X-Real-IP          $remote_addr;
-
-            # optionally, you can adjust the POST request size limit, to allow adding a lot of torrents at once
-            #client_max_body_size 100M;
-
-            # Since v4.2.2, is possible to configure qBittorrent
-            # to set the "Secure" flag for the session cookie automatically.
-            # However, that option does nothing unless using qBittorrent's built-in HTTPS functionality.
-            # For this use case, where qBittorrent itself is using plain HTTP
-            # (and regardless of whether or not the external website uses HTTPS),
-            # the flag must be set here, in the proxy configuration itself.
-            # Note: If this flag is set while the external website uses only HTTP, this will cause
-            # the login mechanism to not work without any apparent errors in console/network resulting in "auth loops".
-            proxy_cookie_path  /                  "/; Secure";
-          '';
         };
       };
 
@@ -126,18 +89,6 @@
         # enableACME = true;
         # forceSSL = true;
 
-        locations = {
-          "^~/phpmysql" = {
-            root = "/var/www/";
-            index = "index.html index.php";
-            extraConfig = ''
-              location ~ \.php$ {
-                include ${pkgs.nginx}/conf/fastcgi.conf;
-                fastcgi_pass unix:${config.services.phpfpm.pools.one.socket};
-              }
-            '';
-          };
-
           "^~/speedtest" = {
             root = "/var/www/";
             index = "index.html index.php";
@@ -160,32 +111,6 @@
               }
             '';
           };
-
-          "^~/torrent/".extraConfig = ''
-            proxy_pass         http://127.0.0.1:8080/;
-            proxy_http_version 1.1;
-
-            proxy_set_header   Host               127.0.0.1:8080;
-            proxy_set_header   X-Forwarded-Host   $http_host;
-            proxy_set_header   X-Forwarded-For    $remote_addr;
-
-            # not used by qBittorrent
-            #proxy_set_header   X-Forwarded-Proto  $scheme;
-            #proxy_set_header   X-Real-IP          $remote_addr;
-
-            # optionally, you can adjust the POST request size limit, to allow adding a lot of torrents at once
-            #client_max_body_size 100M;
-
-            # Since v4.2.2, is possible to configure qBittorrent
-            # to set the "Secure" flag for the session cookie automatically.
-            # However, that option does nothing unless using qBittorrent's built-in HTTPS functionality.
-            # For this use case, where qBittorrent itself is using plain HTTP
-            # (and regardless of whether or not the external website uses HTTPS),
-            # the flag must be set here, in the proxy configuration itself.
-            # Note: If this flag is set while the external website uses only HTTP, this will cause
-            # the login mechanism to not work without any apparent errors in console/network resulting in "auth loops".
-            proxy_cookie_path  /                  "/; Secure";
-          '';
 
           "/favicon.ico".extraConfig = ''access_log off; log_not_found off;'';
         };
