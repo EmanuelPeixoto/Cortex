8ef75a6a84b1802742a3a328541ae4f98627450a
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-01-24 18:09:13 -0300

    SysTray menu added

--- hm/note/ags/Bar/modules/SysTray.tsx
+++ hm/note/ags/Bar/modules/SysTray.tsx
@@ -1,6 +1,4 @@
-import { App } from "astal/gtk3";
 import { bind } from "astal";
-import { Gdk } from "astal/gtk3";
 import Tray from "gi://AstalTray";
 
 export default function SysTray() {
@@ -10,22 +8,20 @@ export default function SysTray() {
     <box className="SysTray">
       {bind(tray, "items").as((items) =>
         items.map((item) => {
-          if (item.iconThemePath) App.add_icons(item.iconThemePath);
-
-          const menu = item.menu?.() || item.create_menu?.();
-
           return (
-            <button className="AAA"
+            <menubutton
               tooltipMarkup={bind(item, "tooltipMarkup")}
-              onDestroy={() => menu?.destroy?.()}
-              onClickRelease={(self) => {
-                menu?.popup_at_widget(self, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null);
-              }}
+              usePopover={false}
+              actionGroup={bind(item, "action-group").as((ag) => [
+                "dbusmenu",
+                ag,
+              ])}
+              menuModel={bind(item, "menu-model")}
             >
-              <icon gIcon={bind(item, "gicon")} />
-            </button>
+              <icon gicon={bind(item, "gicon")} />
+            </menubutton>
           );
-        })
+        }),
       )}
     </box>
   );
