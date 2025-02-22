2cc678b022765091b2c58e02cebd2a01eb14af3d
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-02-22 18:15:37 -0300

    temp, added docker

--- system/note/temp.nix
+++ system/note/temp.nix
@@ -2,9 +2,14 @@
 {
   environment.systemPackages = with pkgs; [
     # chromium
+    docker-compose
   ];
 
   # networking.extraHosts = ''
   #   192.168.0.10   epeixoto.ddns.net
   # '';
+
+  virtualisation.docker.enable = true;
+  users.users.emanuel.extraGroups = [ "docker" ];
+
 }
