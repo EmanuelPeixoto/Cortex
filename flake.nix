8e2d39af16cce31500fee457707fbd3480439dcd
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-04-12 19:31:48 -0300

    sdr added

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     ags.url = "github:aylur/ags";
+    dsdfme.url = "github:lwvmobile/dsd-fme";
     home-manager.url = "github:nix-community/home-manager";
     lexis.url = "github:EmanuelPeixoto/Lexis";
     nix-colors.url = "github:misterio77/nix-colors";
