53a27f846d1d8ffbd208ec4b86110d0571f3d3db
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-04-12 00:44:36 -0300

    added fish, neovim and yazi to system apps

--- system/server/apps.nix
+++ system/server/apps.nix
@@ -2,8 +2,11 @@
 {
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
+    fish                      # Smart and user-friendly command line shell
     git                       # Distributed version control system
+    neovim                    # Vim text editor fork focused on extensibility and agility
     nh                        # Yet another nix cli helper
     nix-output-monitor        # Processes output of Nix commands to show helpful and pretty information
+    yazi                      # Blazing fast terminal file manager written in Rust, based on async I/O
   ];
 }
