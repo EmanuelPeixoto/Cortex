d8d22201c7fdbd85ee8b3cc0ab9e957d601e6efa
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-04-21 12:02:34 -0300

    removed postgres

--- system/server/scti.nix
+++ system/server/scti.nix
@@ -1,17 +1,5 @@
 { pkgs, ... }:
 {
-  services.postgresql = {
-    enable = true;
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database DBuser auth-method
-      local all      all    trust
-      #type database DBuser Address      auth-method
-      host  all      all    127.0.0.1/32 trust
-      host  all      all    ::1/128      trust
-    '';
-  };
-
   environment.systemPackages = with pkgs; [
     docker-compose
   ];
