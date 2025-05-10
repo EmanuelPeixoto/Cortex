149581fc121d50ff0ed01ae7e40170ee1f662542
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2025-05-10 20:24:11 -0300

    added cinnamon, more cpu % on battery

--- system/note/battery.nix
+++ system/note/battery.nix
@@ -8,7 +8,7 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
       CPU_MAX_PERF_ON_AC = 100;
-      CPU_MAX_PERF_ON_BAT = 35;
+      CPU_MAX_PERF_ON_BAT = 60;
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MIN_PERF_ON_BAT = 0;
 
