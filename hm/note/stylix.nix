d454cbbb8ea93254306cb908321df1dc80c2dc8a
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2025-06-25 10:21:15 -0300

    Other themes for Stylix that I liked

--- hm/note/stylix.nix
+++ hm/note/stylix.nix
@@ -13,6 +13,8 @@
     enable = true;
     targets.qt.enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/colors.yaml";
     polarity = "dark";
 
     fonts = {
