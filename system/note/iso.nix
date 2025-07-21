d2a82ea52e302f565f2fc1767c6bee6703728718
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-07-21 12:15:27 -0300

    fixed intent

--- system/note/iso.nix
+++ system/note/iso.nix
@@ -9,5 +9,5 @@
   boot.plymouth.enable = lib.mkForce false;
   networking.networkmanager.enable = lib.mkForce false;
 
-    nixpkgs.hostPlatform = "x86_64-linux";
+  nixpkgs.hostPlatform = "x86_64-linux";
 }
