71c6454b500ea71953dbae5b70d50be74e248ba0
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-08-12 16:43:01 -0300

    multi-thread face recognition

--- system/server/nextcloud.nix
+++ system/server/nextcloud.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
+let
+  occ = "/run/current-system/sw/bin/nextcloud-occ";
+  nextcloudUser = "root";
+  analyzeJobs = 10;
+in
 {
-  # environment.systemPackages = with pkgs; [];
   services.nextcloud = {
     enable = true;
     https = true;
@@ -10,7 +14,7 @@
 
     poolSettings = {
       pm = "dynamic";
-      "pm.max_children" = "40";
+      "pm.max_children" = "60";
       "pm.start_servers" = "6";
       "pm.min_spare_servers" = "4";
       "pm.max_spare_servers" = "12";
@@ -48,15 +52,72 @@
   };
 
   systemd = {
-    timers.simple-timer = {
-      wantedBy = ["timers.target"];
-      partOf = ["nextcloud-facerecognition.service"];
-      timerConfig.OnCalendar = "hourly";
+    services = {
+      nextcloud-face-sync = {
+        description = "Nextcloud Face Recognition Sync Mode";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${occ} face:background_job -u ${nextcloudUser} --sync-mode";
+          User = "nextcloud";
+          Group = "nextcloud";
+        };
+        after = [ "nextcloud-setup.service" ];
+        wants = [ "nextcloud-setup.service" ];
+      };
+
+      nextcloud-face-cluster = {
+        description = "Nextcloud Face Recognition Cluster Mode";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${occ} face:background_job -u ${nextcloudUser} --cluster-mode";
+          User = "nextcloud";
+          Group = "nextcloud";
+        };
+        after = [ "nextcloud-setup.service" ];
+        wants = [ "nextcloud-setup.service" ];
+      };
+
+      # Template para múltiplas instâncias de analyze-mode
+      "nextcloud-face-analyze@" = {
+        description = "Nextcloud Face Recognition Analyze Mode Instance %i";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${occ} face:background_job -u ${nextcloudUser} --analyze-mode";
+          User = "nextcloud";
+          Group = "nextcloud";
+        };
+        after = [ "nextcloud-setup.service" ];
+        wants = [ "nextcloud-setup.service" ];
+      };
+
+      nextcloud-face-analyze-parallel-run = {
+        description = "Run multiple analyze instances in parallel";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = pkgs.writeShellScript "nextcloud-face-parallel" ''
+            for i in $(seq 1 ${toString analyzeJobs}); do
+              systemctl start nextcloud-face-analyze@$i.service &
+            done
+            wait
+          '';
+          User = "root";
+        };
+        after = [ "nextcloud-setup.service" ];
+        wants = [ "nextcloud-setup.service" ];
+      };
     };
 
-    services.nextcloud-facerecognition = {
-      serviceConfig.Type = "oneshot";
-      script = ''/run/current-system/sw/bin/nextcloud-occ face:background_job'';
+    # Timer para disparar análise paralela em N instâncias
+    timers.nextcloud-face-analyze-parallel = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "hourly";
+        Persistent = true;
+      };
+      unitConfig = {
+        Description = "Run Nextcloud face analyze parallel jobs";
+        Requires = "nextcloud-face-analyze-parallel-run.service";
+      };
     };
   };
 }
