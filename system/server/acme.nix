c85329abe086a46ac20dffa4a6e8ed62a8d3e785
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-08-23 19:01:24 -0300

    removed scti, updated flake

--- system/server/acme.nix
+++ system/server/acme.nix
@@ -1,7 +1,7 @@
 {
   security.acme = {
     acceptTerms = true;
-    defaults.email = "peixoto_emanuel@hotmail.com";
+    defaults.email = "leunamepeixoto@gmail.com";
     defaults.webroot = "/var/lib/acme/acme-challenge";
     certs."epeixoto.ddns.net" = {
       email = "leunamepeixoto@gmail.com";
