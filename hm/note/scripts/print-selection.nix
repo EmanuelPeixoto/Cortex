bf25bfc693ddc6c2c0973080257441e759c6a555
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-21 06:54:22 -0300

    changed grim to grimblast

--- hm/note/scripts/print-selection.nix
+++ hm/note/scripts/print-selection.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "print-selection" ''
-  ${pkgs.grim}/bin/grim -l 0 -g "$(${pkgs.slurp}/bin/slurp -d -w 0)" - | ${pkgs.swappy}/bin/swappy -f -
+  ${pkgs.grimblast}/bin/grimblast save area - | ${pkgs.swappy}/bin/swappy -f -
 ''
