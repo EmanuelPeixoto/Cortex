16f4b5696df020cd26d0401cfc3388353965521e
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-26 20:36:04 -0300

    added rstudio packages

--- system/note/rstudio.nix
+++ system/note/rstudio.nix
@@ -1,11 +1,49 @@
 { pkgs, ... }:
 
 let
+  # para instalar os pacotes por aqui, busque os disponiveis no nixpkgs ex.: rPackages.BiocManager e coloque apenas BiocManager abaixo.
   myRPackages = with pkgs.rPackages; [
     BiocManager
-    stringr
+    DiagrammeR
+    DiagrammeRsvg
+    Matrix
+    UCSCXenaShiny
+    UCSCXenaTools
+    UpSetR
+    VIM
+    caret
+    circlize
+    data_table
+    doParallel
+    foreach
+    fs
+    ggplot2
+    ggpubr
+    ggtext
+    gridExtra
+    kableExtra
+    lightgbm
+    magick
+    mice
+    missForest
+    openxlsx
+    pROC
+    pdftools
+    remotes
+    rentrez
+    reshape2
+    rio
+    rms
+    rsvg
+    stringi
+    survival
+    survivalROC
+    survminer
+    tidyverse
+    tiff
+    timeROC
+    xgboost
   ];
-
 in
 {
   services.rstudio-server = {
@@ -19,19 +57,33 @@ in
 
   systemd.services.rstudio-server.path = with pkgs; [
     bash
-    coreutils
-    gnumake
-    gcc
-    gfortran
-    pkg-config
-    zlib.dev
-    libxml2.dev
-    openssl.dev
-    curl.dev
-    libgit2
-    eigen
-    imagemagick
-    fontconfig
+    binutils
     cairo
+    cmake
+    coreutils
+    curl.dev
+    diffutils
+    eigen
+    fontconfig.dev
+    freetype.dev
+    gawk
+    gcc
+    getconf
+    gfortran
+    gnutar
+    gnumake
+    gzip
+    imagemagick.dev
+    libgit2
+    libjpeg
+    libpng.dev
+    librsvg
+    libtiff
+    libxml2.dev
+    nlopt
+    openssl.dev
+    pkg-config
+    poppler
+    zlib.dev
   ];
 }
