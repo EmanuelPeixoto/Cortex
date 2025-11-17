5535e436c0d9a3411c4db38ee42cc6538504edec
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-11-16 21:46:20 -0300

    WIP Sunshine

--- system/server/sunshine.nix
+++ system/server/sunshine.nix
@@ -1,71 +1,39 @@
 { pkgs, ... }:
 {
-  users.users."emanuel".extraGroups = [ "input" "video" ];
+  services.sunshine = {
+    enable = true;
+    package = pkgs.stable.sunshine.override { cudaSupport = true; };
+    openFirewall = true;
+  };
+
+  # Isso ainda é uma boa prática
+  services.udev.packages = [
+    pkgs.sunshine
+  ];
+
+  # boot.kernelParams = [ "video=DP-8:1920x1080R@60D" ];
+
+  # 1. Adicione "uinput" aqui
+  # users.users."emanuel".extraGroups = [ "input" "video" "render" "uinput" ];
 
   environment.systemPackages = [
-    pkgs.sunshine # Adicionamos o pacote aqui
-    pkgs.wineWowPackages.stable # Manteve o Wine 32-bit
+    pkgs.sunshine
+    pkgs.wine
   ];
 
-  networking.firewall.allowedTCPPorts = [
-    47984 # Sunshine Web UI (HTTPS)
-    47989 # Sunshine Web UI (HTTP)
-    47990 # Controle
-    48010 # Streaming de Vídeo
-  ];
-  networking.firewall.allowedUDPPorts = [
-    47998 # Descoberta
-    47999 # Descoberta
-    48000 # Estatísticas
-    48002 # Estatísticas
-    48010 # Streaming de Áudio
-  ];
+  services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = "emanuel";
+    };
+    defaultSession = "none+icewm";
+  };
 
-  services.displayManager.defaultSession = "none+icewm";
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "emanuel";
 
   services.xserver = {
     enable = true;
+    videoDrivers = [ "nvidia" ];
     windowManager.icewm.enable = true;
     displayManager.lightdm.enable = true;
-    videoDrivers = [ "nvidia" ];
-    config = ''
-      Section "ServerLayout"
-        Identifier "Layout0"
-        Screen 0 "Screen0" 0 0
-      EndSection
-
-      Section "Device"
-        Identifier "Device0"
-        Driver     "nvidia"
-        VendorName "NVIDIA Corporation"
-        Option     "AllowEmptyInitialConfiguration" "true"
-      EndSection
-
-      Section "Screen"
-        Identifier "Screen0"
-        Device     "Device0"
-        Monitor    "Monitor0"
-        DefaultDepth 24
-        SubSection "Display"
-          Depth 24
-          Virtual "1920 1080"
-        EndSubSection
-      EndSection
-
-      Section "Monitor"
-        Identifier "Monitor0"
-        HorizSync 28.0 - 80.0
-        VertRefresh 50.0 - 75.0
-      EndSection
-    '';
-
-    # 4. A CORREÇÃO MÁGICA (A OPÇÃO CORRETA)
-    # Isto inicia o Sunshine DENTRO da sessão 'icewm' do 'emanuel'
-    # services.xserver.windowManager.icewm.startup = ''
-    # ${pkgs.sunshine}/bin/sunshine &
-    # '';
-
   };
 }
