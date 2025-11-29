b6d2db66cb1ca1f4a046ec8a4c98aac4ec874c4d
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-11-29 18:35:14 -0300

    compacted flake

--- flake.nix
+++ flake.nix
@@ -29,61 +29,56 @@
           (final: prev: {
             stable = import nixpkgs-stable {
               inherit system;
-              config = prev.config;
+              inherit (prev) config;
             };
           })
         ];
       };
+
+      # Lista dos sistemas para gerar configurações
+      systems = [ "note" "server" ];
+
+      # Função para gerar uma configuração NixOS
+      mkNixosSystem = name:
+        nixpkgs.lib.nixosSystem {
+          inherit pkgs;
+          modules = [
+            ./system/${name}
+            home-manager.nixosModules.home-manager
+          ];
+          specialArgs = { inherit inputs; };
+        };
+      
+      # Função para gerar uma configuração do Home Manager
+      mkHomeConfig = name:
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./hm/${name} ];
+          extraSpecialArgs = { inherit inputs; };
+        };
+
     in {
-      nixosConfigurations = {
-        NixOS-Note = nixpkgs.lib.nixosSystem {
-          inherit pkgs;
-          modules = [
-            ./system/note
-            home-manager.nixosModules.home-manager
-          ];
-          specialArgs = { inherit inputs; };
+      nixosConfigurations =
+        (nixpkgs.lib.genAttrs systems (name: mkNixosSystem name)) // {
+          # Caso especial para a ISO
+          NixOS-Note-ISO = nixpkgs.lib.nixosSystem {
+            inherit pkgs;
+            modules = [
+              ./system/note/iso.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.emanuel = import ./hm/note;
+                  extraSpecialArgs = { inherit inputs; };
+                };
+              }
+            ];
+            specialArgs = { inherit inputs; };
+          };
         };
 
-        NixOS-Note-ISO = nixpkgs.lib.nixosSystem {
-          inherit pkgs;
-          modules = [
-            ./system/note/iso.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.emanuel = import ./hm/note;
-                extraSpecialArgs = { inherit inputs; };
-              };
-            }
-          ];
-          specialArgs = { inherit inputs; };
-        };
-
-        NixOS-Server = nixpkgs.lib.nixosSystem {
-          inherit pkgs;
-          modules = [
-            ./system/server
-            home-manager.nixosModules.home-manager
-          ];
-          specialArgs = { inherit inputs; };
-        };
-      };
-
-      homeConfigurations = {
-        note = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./hm/note ];
-          extraSpecialArgs = { inherit inputs; };
-        };
-
-        server = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./hm/server ];
-          extraSpecialArgs = { inherit inputs; };
-        };
-      };
+      homeConfigurations = nixpkgs.lib.genAttrs systems (name: mkHomeConfig name);
     };
 }
