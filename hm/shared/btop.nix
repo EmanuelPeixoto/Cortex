d3971d4b1a175ecec66cbd4e734b23b8b36ee955
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-12-28 13:56:11 -0300

    btop auto change pkg

--- hm/shared/btop.nix
+++ hm/shared/btop.nix
@@ -1,8 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, lib, osConfig ? null, ... }:
+let
+  videoDrivers = if osConfig != null
+    then osConfig.services.xserver.videoDrivers
+  else [];
+
+  isNvidia = lib.lists.elem "nvidia" videoDrivers;
+  isAmd    = lib.lists.elem "amdgpu" videoDrivers;
+
+  btopPackage = if isNvidia then pkgs.btop-cuda
+  else if isAmd then pkgs.btop-rocm
+  else pkgs.btop;
+in
 {
   programs.btop = {
     enable = true;
-    package = pkgs.btop-cuda;
+    package = btopPackage;
     settings = {
       update_ms = 500;
       cpu_single_graph = true;
