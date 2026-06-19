b36f347ef052a2788f198b547bfededbdd9d176b
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-06-19 11:22:42 -0300

    quickshell: silence xdg-portal app id warning

--- hm/note/quickshell.nix
+++ hm/note/quickshell.nix
@@ -19,6 +19,13 @@ in
       ExecStart = "${CustomQS}/bin/quickshell";
       Restart = "no";
       KillMode = "process";
+      Environment = [
+        "QT_QPA_PLATFORM=wayland"
+        "QT_APPLICATION_NAME=quickshell"
+        "DESKTOP_ENTRY=quickshell"
+        "XDP_IGNORE_APP_ID=1"
+        "QT_LOGGING_RULES=qt.qpa.services.warning=false"
+      ];
     };
     Install.WantedBy = ["hyprland-session.target"];
   };
