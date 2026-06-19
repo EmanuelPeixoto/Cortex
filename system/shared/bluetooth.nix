e40ba3da369603d032c17435d91a4213af40995a
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-06-19 11:22:47 -0300

    users: add avahi group for mDNS access

--- system/shared/bluetooth.nix
+++ system/shared/bluetooth.nix
@@ -1,4 +1,17 @@
 {
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      ControllerMode = "dual";
+    };
+    Policy = {
+      AutoEnable = true;
+    };
+  };
   services.blueman.enable = true;
+
+  systemd.services.bluetooth.serviceConfig = {
+    StandardOutput = "null";
+    StandardError = "null";
+  };
 }
