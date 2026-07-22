772c4ec06708cd287d60c84a685fda4ac5987802
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-07-22 11:28:30 -0300

    Removed unused apps, undone comments

--- hm/note/apps.nix
+++ hm/note/apps.nix
@@ -12,63 +12,54 @@ in
     motd
     yt-tlp-menu
 
-    # aircrack-ng               # Wireless encryption cracking tools
-    # android-tools             # Android SDK platform tools
+    aircrack-ng               # Wireless encryption cracking tools
     audacity                  # Sound editor with graphical UI
-    # blender                   # 3D Creation/Animation/Publishing System
     curl                      # Command line tool for transferring files with URL syntax
     # davinci-resolve           # Professional video editing, color, effects and audio post-processing
     discord                   # All-in-one cross-platform voice and text chat for gamers
-    # exfatprogs                # exFAT filesystem userspace utilities
-    # f3                        # Fight Flash Fraud
+    exfatprogs                # exFAT filesystem userspace utilities
     ffmpeg                    # Complete, cross-platform solution to record, convert and stream audio and video
-    # gcc                       # GNU Compiler Collection
-    # gdb                       # GNU Project debugger
-    # gemini-cli                # AI agent that brings the power of Gemini directly into your terminal
-    # gh                        # GitHub CLI tool
-    # gimp                      # GNU Image Manipulation Program
-    # glibc                     # GNU C Library
-    # gnumake                   # Tool to control the generation of non-source files from sources
-    # gparted                   # Graphical disk partitioning tool
-    # iftop                     # Display bandwidth usage on a network interface
-    # inetutils                 # Collection of common network programs
-    # iw                        # Tool to use nl80211
-    # jmtpfs                    # FUSE filesystem for MTP devices like Android phones
+    gcc                       # GNU Compiler Collection
+    gdb                       # GNU Project debugger
+    gimp                      # GNU Image Manipulation Program
+    glibc                     # GNU C Library
+    gnumake                   # Tool to control the generation of non-source files from sources
+    iftop                     # Display bandwidth usage on a network interface
+    inetutils                 # Collection of common network programs
+    iw                        # Tool to use nl80211
+    jmtpfs                    # FUSE filesystem for MTP devices like Android phones
     killall                   # No description :/
     lazygit                   # Simple terminal UI for git commands
-    # librepcb                  # Free EDA software to develop printed circuit boards
-    # lm_sensors                # Tools for reading hardware sensors
-    # lshw                      # Provide detailed information on the hardware configuration of the machine
-    # metasploit                # Metasploit Framework - a collection of exploits
-    # mpv                       # General-purpose media player, fork of MPlayer and mplayer2
+    lm_sensors                # Tools for reading hardware sensors
+    lshw                      # Provide detailed information on the hardware configuration of the machine
+    metasploit                # Metasploit Framework - a collection of exploits
     ncdu                      # Disk usage analyzer with an ncurses interface
-    # netcat                    # Free TLS/SSL implementation
-    # nload                     # Monitors network traffic and bandwidth usage with ncurses graphs
-    # nmap                      # Free and open source utility for network discovery and security auditing
-    # nomacs                    # Qt-based image viewer
-    # openssl                   # Cryptographic library that implements the SSL and TLS protocols
-    # pciutils                  # Collection of programs for inspecting and manipulating configuration of PCI devices
+    netcat                    # Free TLS/SSL implementation
+    nload                     # Monitors network traffic and bandwidth usage with ncurses graphs
+    nmap                      # Free and open source utility for network discovery and security auditing
+    nomacs                    # Qt-based image viewer
+    openssl                   # Cryptographic library that implements the SSL and TLS protocols
+    pciutils                  # Collection of programs for inspecting and manipulating configuration of PCI devices
     platformio                # Open source ecosystem for IoT development
     # prismlauncher             # Free, open source launcher for Minecraft
-    # progress                  # Tool that shows the progress of coreutils programs
+    progress                  # Tool that shows the progress of coreutils programs
     pwvucontrol               # Pipewire Volume Control
-    # qbittorrent               # Featureful free software BitTorrent client
-    # qemu                      # Generic and open source machine emulator and virtualizer
+    qbittorrent               # Featureful free software BitTorrent client
+    qemu                      # Generic and open source machine emulator and virtualizer
     qpwgraph                  # Qt graph manager for PipeWire, similar to QjackCtl
-    # rustup                    # Rust toolchain installer
-    # speedtest-cli             # Command line interface for testing internet bandwidth using speedtest.net
-    # stress                    # Simple workload generator for POSIX systems.
-    # telegram-desktop          # Telegram Desktop messaging app
+    speedtest-cli             # Command line interface for testing internet bandwidth using speedtest.net
+    stress                    # Simple workload generator for POSIX systems.
+    telegram-desktop          # Telegram Desktop messaging app
     # texliveFull               # TeX Live environment
-    # tor-browser               # Privacy-focused browser routing traffic through the Tor network
-    # unrar                     # Utility for RAR archives
-    # unzip                     # Extraction utility for archives compressed in .zip format
-    # usbutils                  # Tools for working with USB devices, such as lsusb
+    tor-browser               # Privacy-focused browser routing traffic through the Tor network
+    unrar                     # Utility for RAR archives
+    unzip                     # Extraction utility for archives compressed in .zip format
+    usbutils                  # Tools for working with USB devices, such as lsusb
     vlc                       # Cross-platform media player and streaming server
-    # wget                      # Tool for retrieving files using HTTP, HTTPS, and FTP
+    wget                      # Tool for retrieving files using HTTP, HTTPS, and FTP
     wl-clipboard              # Command-line copy/paste utilities for Wayland
     yt-dlp                    # Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
     zathura                   # Highly customizable and functional PDF viewer
-    # zip                       # Compressor/archiver for creating and modifying zipfiles
+    zip                       # Compressor/archiver for creating and modifying zipfiles
   ];
 }
