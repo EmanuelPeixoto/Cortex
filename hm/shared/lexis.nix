ae18821770da5f545818534d443d9a931a47b977
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-08-09 13:51:50 -0300

    minimal Lexis config

--- hm/shared/lexis.nix
+++ hm/shared/lexis.nix
@@ -1,6 +1,27 @@
-{ inputs, ... }:
+{ inputs, pkgs, lib, ... }:
+let
+  nixvim = inputs.lexis.inputs.nixvim;
+in
 {
   home.packages = [
-    inputs.lexis.packages."x86_64-linux".default
+    (nixvim.legacyPackages.${pkgs.system}.makeNixvimWithModule {
+      inherit pkgs;
+      module = {
+        imports = [ (import (inputs.lexis + "/config")) ];
+        # only C/C++ (clangd) and Nix (nixd)
+        plugins.vimtex.enable = lib.mkForce false;
+        plugins.lsp.servers = {
+          bashls.enable = lib.mkForce false;
+          cssls.enable = lib.mkForce false;
+          emmet_ls.enable = lib.mkForce false;
+          gopls.enable = lib.mkForce false;
+          html.enable = lib.mkForce false;
+          jsonls.enable = lib.mkForce false;
+          rust_analyzer.enable = lib.mkForce false;
+          texlab.enable = lib.mkForce false;
+          ts_ls.enable = lib.mkForce false;
+        };
+      };
+    })
   ];
 }
