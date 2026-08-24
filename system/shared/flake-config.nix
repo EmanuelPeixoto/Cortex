eaf2ae9775d43744cfe697fcef06b824b228273c
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-08-24 15:13:20 -0300

    flake: update nixpkgs, enable i686-linux builds for Steam
    nix flake update to pick up Nextcloud 34.0.3, which fixes the
    "More than 1000 expressions in a list are not allowed on Oracle"
    error triggered by large folder scans.
    
    The new nixpkgs revision no longer ships the 32-bit NVIDIA graphics
    packages (eglexternalplatform, egl-gbm, nvidia-egl-external-platforms-x32)
    in cache.nixos.org for i686-linux, so enable i686-linux as an extra
    platform so Steam's 32-bit stack builds locally.

--- system/shared/flake-config.nix
+++ system/shared/flake-config.nix
@@ -1,5 +1,8 @@
 { config, ... }:
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-platforms = [ "i686-linux" ];
+  };
   environment.sessionVariables.NH_FLAKE = "${config.users.users.emanuel.home}/.config/Cortex";
 }
