3b7aaf2a1f016a69fa7d26c5247e8b431a8003c8
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-09-08 23:41:36 -0300

    mpd: require password for remote clients, keep local access open

--- hm/note/mpd.nix
+++ hm/note/mpd.nix
@@ -6,6 +6,13 @@
     network.listenAddress = "any";
     extraConfig = ''
       zeroconf_enabled "no"
+
+      # Remote clients (phone) need a password; local clients (rmpc, mpris)
+      # keep full access without one. See ~/.config/mpd/secret.conf.
+      default_permissions "read"
+      local_permissions "read,add,control,admin"
+      include_optional "${config.home.homeDirectory}/.config/mpd/secret.conf"
+
       audio_output {
       type "pipewire"
       name "MPD pipewire"
